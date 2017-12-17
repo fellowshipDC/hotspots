c2c404b16bc52f03f7f3be93527956bcb2f212fc
Jorge Andrade <jorandradefig@gmail.com>
2017-12-17 16:24:55 -0600

    Update google map

--- app/src/app/gmap/gmap.component.ts
+++ app/src/app/gmap/gmap.component.ts
@@ -12,115 +12,66 @@ declare const MarkerClusterer: any;
 })
 export class GmapComponent implements OnInit {
 
-  lat: number = 19.386793;
-  lng: number =  -99.141562;
   dataset: any = [];
+  marker;
 
-  constructor() {
-    
-   }
+  constructor() {}
+
+  initMap() {
+    const center = { lat: 19.4286973, lng: -99.156051 };
+
+    const map = new google.maps.Map(document.getElementById('gmap'), {
+      zoom: 12,
+      center: center
+    });
+
+    return map;
+  }
+
+  addMarker(map, lat, lng) {
+    const position = new google.maps.LatLng(lat, lng);
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend(position);
+    const marker = new google.maps.Marker({
+        position: position,
+        map: map
+    });
+
+    marker.addListener('click', () => {
+      map.setCenter(marker.getPosition());
+      map.setZoom(19);
+      this.marker = marker;
+    });
+
+    return marker;
+  }
+
   ngOnInit() {
+
+    const markers = [];
 
     d3.csv('./assets/hotspotscut.csv', (data) => {
       this.dataset = data;
-      
-      function initMap() {
-        
-  
-        var center = {lat: 19.386793, lng: -99.141562};
-        var bounds = new google.maps.LatLngBounds();
-        console.log(data)
-  
-        var map = new google.maps.Map(document.getElementById('gmap'), {
-          zoom: 12,
-          center: center
-        });
 
-        var infowindow = new google.maps.InfoWindow(/*{pixelOffset: new google.maps.Size(700,300)
-      }*/);
-  
-        
-      
-        
-        var markers = []
-        for(var i = 0; i < data.length; i++ ) {
-          var position = new google.maps.LatLng(data[i]['latitud'], data[i]['longitud']);
-          bounds.extend(position);
-          var marker = new google.maps.Marker({
-              position: position,
-              map: map,
-             
-          
-              
-          });
+      console.log(this.dataset);
 
-         var info = "<div class='content'>" + "<b>" + data[i]['nombre'] +"</b>" + 
-         "<br>" + 
-         "<p> <b>Dirección: </b></p> " + data[i]['vialidad'] + " " +
-         data[i]['calle'] + '<br>'  + " Ext. " + data[i]['numero_exterior'] + " Int. " + data[i]['numero_interior'] 
-        +"<br><br>"+"<p> <b>Estatus: </b></p> "+ data[i]['estatus']  + '<br><br>' + "<p> <b>Conectividad: </b></p> " +  data[i]['conectividad'] +
-       '<form id="mapform">' +
-        
-        '<div class="form-group">' +
-           '<label for="exampleInputPassword1">Comentarios</label>' +
-          '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Comentarios del punto de acceso">' +
-        ' </div>' +
-        
-        ' <div class="form-group">' +
-          '<label for="exampleInput">Calificación</label><br>' +
-          '<span class="rating">'+
-             '<input type="radio" id="star5" name="rating" value=5 /><label class = "full" for="star5" title="Awesome - 5 stars"></label>' +
-             '<input type="radio" id="star4" name="rating" value=4 /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>' +
-             '<input type="radio" id="star3" name="rating" value=3 /><label class = "full" for="star3" title="Meh - 3 stars"></label>' +
-             '<input type="radio" id="star2" name="rating" value=2 /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>' +
-             '<input type="radio" id="star1" name="rating" value=1 /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>'+
-            
-             '</span>' +
-         '</div> <br><br><br>' +
-         
-        '<button id="subbtn" type="submit" class="btn btn-light">Submit</button>'+
-       '</form>' +
-     
-     '</div>' + "</div>"
-          
-         
-        markers.push(marker);           
-          
-            markers.forEach(function(marker){
+      const map = this.initMap();
 
-            marker.addListener('click', function() {
-//esto es lo que agregue y mi mapa desaparecio
-                    // google.maps.event.addListener(infowindow, 'domready', function() {
-                    //   document.getElementById("map-form").addEventListener("submit", function(e) {
-                    //       (<any>event).stop();
-                    //       console.log("hi!"); });  });
-                       //hasta aqui   
-
-              infowindow.setContent(info);
-              infowindow.open(map,marker)  ;   
-            map.setCenter(marker.getPosition());
-            map.setZoom(19);
-              });
-
-             
-              
+      this.dataset.map((data) => {
+        const marker = this.addMarker(map, data.latitud, data.longitud);
+        markers.push(marker);
       });
-        
-      google.maps.event.addListener(map, 'click', function() {
-        infowindow.close();
-        map.setCenter(center);
-        map.setZoom(11);
-      }); 
-         
-      }
-      var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      
-    }
-    
-    
-      initMap();
-     
+
+      const markerCluster = new MarkerClusterer(
+        map,
+        markers,
+        {
+          imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        }
+      );
 
     });
-}}
 
+  }
+
+}
